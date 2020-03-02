5d12da052d2f509b39457387c60e2cb11b4b1d47
Bobby Schade <bschade.18@gmail.com>
2020-03-02 14:59:05 -0600

    Updates

---
+++ precache-manifest.8ab2a85f12044bc5c38ba2e2ff2312e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff736c9a53bf2c17bbc9f6b1f188caa5",
+    "url": "/tictactoev2/index.html"
+  },
+  {
+    "revision": "275f0a843c2e0249d64c",
+    "url": "/tictactoev2/static/css/main.062f6f86.chunk.css"
+  },
+  {
+    "revision": "27cc5f2a98285c2e6dd1",
+    "url": "/tictactoev2/static/js/2.54b2104f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tictactoev2/static/js/2.54b2104f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275f0a843c2e0249d64c",
+    "url": "/tictactoev2/static/js/main.e06cdde1.chunk.js"
+  },
+  {
+    "revision": "3ae29b9c028286161a44",
+    "url": "/tictactoev2/static/js/runtime-main.54848808.js"
+  }
+]);
